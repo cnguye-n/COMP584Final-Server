2f48daad633646f2c720a34924a68245bb19a227
cnguye-n <52482302+cnguye-n@users.noreply.github.com>
2025-12-17 23:41:48 -0800

    update

--- COMP584Server/Data/DTO/LoginResponse.cs
+++ COMP584Server/Data/DTO/LoginResponse.cs
@@ -5,6 +5,8 @@
         public bool Success {get; set; }
         public string Message {get; set; } = string.Empty;
         public string Token { get; set; } = string.Empty ;
+        //token expiration so clients can handle session timing and reauthentication
+        public DateTime Expiration { get; set; }
 
     }
 }
